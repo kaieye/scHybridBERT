a2fc98a6874fb25538de9778152a966d0dcacf4c
Chos1nz <74253784+kaieye@users.noreply.github.com>
2023-09-27 08:12:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 for i in   "Mouse" "Zeisel" "Klein" "AD-brain" "PBMC-4k" "PBMC-7k" "PBMC-Kang-A" "PBMC-Kang-B" "PBMC-Kang-C"
 do
-    python3 -u preprocess.py --data_name $i
+    python3 -u Preprocess.py --data_name $i
     python3 -u Gene_L.py --data_name $i --regu_embed 1  >> ${i}.log 2 >&1
     python3 -u Cell_L.py --data_name $i --dis_method 3  >> ${i}.log 2 >&1
     python3 -u fusion.py --data_name $i>> ${i}.log 2 >&1
